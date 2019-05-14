2c9bda30bb2f1bc598cc9acaf0da4523c7af09d4
zouxiangyun21586 <1171902313@qq.com>
2019-05-14 21:33:28 +0800

    提交

---
+++ src/main/java/test/githubTest/A.java
@@ -0,0 +1,23 @@
+package test.githubTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class A 
+{
+    public static void main( String[] args )
+    {
+    	A.aa();
+        System.out.println( "Hello World!" );
+    }
+    
+    public static void aa() {
+    	int a = 1;
+    	int b = 2;
+    	int c = a + b;
+    }
+    public void testA() {
+    	System.out.println("修改了代码内容");
+    }
+}
